7aeba98447ceb37c506dbc39a8367d9dea0984bf
Cinthya Adriana Mendez <96146911+CinthyaAdriana@users.noreply.github.com>
2022-01-03 14:07:24 -0600

    carpeta pagina cv

---
+++ javascript/index.js
@@ -0,0 +1,34 @@
+
+//Animaciones en Scroll
+window.sr = ScrollReveal();
+sr.reveal(".circulo",{
+  duration: 3000 
+}); 
+
+window.sr = ScrollReveal();
+sr.reveal(".barra",{
+ duration: 3000, 
+ origin:"bottom", 
+  distance:"-1000px"
+}); 
+//Animaciones en Scroll Parrafos 
+window.sr = ScrollReveal();
+sr.reveal(".contenidoparrafo1",{
+ duration: 4000, 
+ origin:"right", 
+  distance:"400px"
+}); 
+
+window.sr = ScrollReveal();
+sr.reveal(".contenidoparrafo2",{
+ duration: 4000, 
+ origin:"left", 
+  distance:"400px"
+});  
+
+window.sr = ScrollReveal();
+sr.reveal(".contenidoparrafo4",{
+ duration: 4000, 
+ origin:"left", 
+  distance:"400px"
+});  
